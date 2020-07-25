0b4b015c381696f2fbeaca3d664e4ccd06f160cd
rrozen <ronen.rozen@skyboxsecurity.com>
2020-07-25 16:20:17 +0300

    init commit

---
+++ FinalProjectDotNet/Pages/Register.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalProjectDotNet.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            Console.WriteLine(Request.Form);
+        }
+    }
+}
