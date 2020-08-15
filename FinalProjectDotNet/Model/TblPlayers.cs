db35e8c109da774b13a7ca762bbe22a5832d109e
rrozen <ronen.rozen@skyboxsecurity.com>
2020-08-15 18:17:56 +0300

    Create player crud

--- FinalProjectDotNet/Model/TblPlayers.cs
+++ FinalProjectDotNet/Model/TblPlayers.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FinalProjectDotNet.Model
 {
-    public class User
+    public class TblPlayers
     {
-        [Required(ErrorMessage = "Email is mandatory")]
+        public int Id { get; set; }
         public string Email { get; set; }
-        public string password { get; set; }
-
+        public string Password { get; set; }
     }
 }
--- FinalProjectDotNet/Startup.cs
+++ FinalProjectDotNet/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using FinalProjectDotNet.Data;
+
 
 namespace FinalProjectDotNet
 {
@@ -24,6 +27,10 @@ namespace FinalProjectDotNet
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<PlayerDataContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("PlayerDataContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
