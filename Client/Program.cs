2efc0289a3fe23196fc5f06695c99733bafad182
Regev <regev81@gmail.com>
2020-09-05 21:11:33 +0300

    Add HttpClient

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,6 +12,7 @@ namespace Client
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        public static HttpClient client = new HttpClient();
         [STAThread]
         static void Main()
         {
